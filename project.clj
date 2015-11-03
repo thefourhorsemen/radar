aea9d5fe774d3aaf6e9694a615104569e202bc49
Frederic Bourgine <frederic.bourgine@gmail.com>
2015-11-03 23:15:12 +0100

    Using quil for 2D drawing.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-json "0.5.3"]]
+                 [clj-json "0.5.3"]
+                 [quil "2.2.6"]]
   :main ^:skip-aot radar.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/radar/core.clj
+++ src/radar/core.clj
@@ -1,11 +1,25 @@
 (ns radar.core
   (:require [clj-json.core :as json]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [quil.core :as q])
   (:gen-class))
 
 (defn read-configuration [filename]
   (json/parse-string (slurp filename)))
 
+(defn draw []
+  ; make background white
+  (q/background 255)
+  (let [xcenter (/ (q/width) 2)
+        ycenter (/ (q/height) 2)]
+    (doseq [t (reverse (range 1 (inc 4)))]
+      (q/ellipse xcenter ycenter (* 200 t) (* 200 t)))
+    (q/line xcenter 0 xcenter (q/height))
+    (q/line 0 ycenter (q/width) ycenter)))
+0
 (defn -main [& args]
-  (let [filename (first args)]
-    (println (read-configuration filename))))
+  (let [filename (first args)
+        configuration (read-configuration filename)]
+
+    ; run sketch
+    (q/defsketch radar :size [900 900] :draw draw)))
