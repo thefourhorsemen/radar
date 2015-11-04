891dd6691c1ea282064f0c4c4154ae39e53fa163
Frederic Bourgine <frederic.bourgine@gmail.com>
2015-11-05 00:02:01 +0100

    Adding text from configuration.

--- src/radar/core.clj
+++ src/radar/core.clj
@@ -7,19 +7,40 @@
 (defn read-configuration [filename]
   (json/parse-string (slurp filename)))
 
-(defn draw []
+(defn targets [configuration]
+  (map #(% "name") configuration))
+
+(defn center []
+  [(/ (q/width) 2)
+   (/ (q/height) 2)])
+
+(defn draw-circle [configuration]
   ; make background white
-  (q/background 255)
-  (let [xcenter (/ (q/width) 2)
-        ycenter (/ (q/height) 2)]
-    (doseq [t (reverse (range 1 (inc 4)))]
-      (q/ellipse xcenter ycenter (* 200 t) (* 200 t)))
+  ;  (q/background 255)
+  (let [[xcenter ycenter] (center)]
+    (doseq [t (reverse (range 1 (inc (count configuration))))]
+      (q/ellipse xcenter ycenter (* 200 t) (* 200 t)))))
+
+(defn draw-axis []
+  (let [[xcenter ycenter] (center)]
     (q/line xcenter 0 xcenter (q/height))
     (q/line 0 ycenter (q/width) ycenter)))
-0
+
+(defn draw-targets [targets]
+  ;  (q/fill 0)
+  (q/text-size 20)
+  (q/text (nth targets 0) 0 0)
+  (q/text (nth targets 1) (q/width) 0)
+  (q/text (nth targets 2) (q/width) (q/height))
+  (q/text (nth targets 3) 0 (q/height)))
+
+(defn draw-configuration [configuration]
+  (draw-circle configuration)
+  (draw-axis)
+  (draw-targets (targets configuration)))
+
 (defn -main [& args]
   (let [filename (first args)
         configuration (read-configuration filename)]
-
     ; run sketch
-    (q/defsketch radar :size [900 900] :draw draw)))
+    (q/defsketch radar :size [900 900] :draw (partial draw-configuration configuration))))
--- test/radar/core_test.clj
+++ test/radar/core_test.clj
@@ -2,29 +2,36 @@
   (:require [clojure.test :refer :all]
             [radar.core :refer :all]))
 
+(def sample-configuration
+  [{"name" "Robots"
+    "categories" {"adopt" "Ar Drone"
+                  "trial" "Phamtom X Hexapod"
+                  "assess" "Myo Armband"
+                  "hold" "Roomba"}
+    }
+   {"name" "Languages"
+    "categories" {"adopt" "Clojure"
+                  "trial" "Idris"
+                  "assess" "Pixie"
+                  "hold" "JavaScript"}
+    }
+   {"name" "Cute Animals"
+    "categories" {"adopt" "Llamas"
+                  "trial" "Alpagas"
+                  "assess" "Wombats"
+                  "hold" "Hedgehogs"}
+    }
+   {"name" "Tasty Food"
+    "categories" {"adopt" "Crumpets"
+                  "trial" "Mint Tim Tams"
+                  "assess" "Raclette"
+                  "hold" "Egg Nog"}
+    }])
+
 (deftest test-read-configuration
-  (let [expected [{"name" "Robots"
-                   "categories" {"adopt" "Ar Drone"
-                                 "trial" "Phamtom X Hexapod"
-                                 "assess" "Myo Armband"
-                                 "hold" "Roomba"}
-                   }
-                  {"name" "Languages"
-                   "categories" {"adopt" "Clojure"
-                                 "trial" "Idris"
-                                 "assess" "Pixie"
-                                 "hold" "JavaScript"}
-                   }
-                  {"name" "Cute Animals"
-                   "categories" {"adopt" "Llamas"
-                                 "trial" "Alpagas"
-                                 "assess" "Wombats"
-                                 "hold" "Hedgehogs"}
-                   }
-                  {"name" "Tasty Food"
-                   "categories" {"adopt" "Crumpets"
-                                 "trial" "Mint Tim Tams"
-                                 "assess" "Raclette"
-                                 "hold" "Egg Nog"}
-                   }]]
+  (let [expected sample-configuration]
     (is (= expected (read-configuration "sample.json")))))
+
+(deftest test-targets
+  (let [expected ["Robots" "Languages" "Cute Animals" "Tasty Food"]]
+    (is (= expected (targets sample-configuration)))))
